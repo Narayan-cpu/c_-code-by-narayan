8e65d200804e5e3089559fe183aee3ecab9621db
codeguru <naiknarayanp557@gmail.com>
2023-05-11 09:50:39 +0530

    Create addition.c
    IT'S A PROGRAM TO ADD 2 NUMBERS
    SIMPLE PROGRAM

---
+++ gitcprogram/addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter any number to be added :",a);
+    scanf("%d",&a);
+    printf("enter number to be added :",b);
+    scanf("%d",&b);
+    printf("The sum of two number: %d",a+b);
+}
