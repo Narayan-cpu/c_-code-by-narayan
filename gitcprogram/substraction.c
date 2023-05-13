7b3095748698d7bad5c4c5227c4d00e402a917ee
narayan <naiknarayanp557@gmail.com>
2023-05-13 10:47:32 +0530

    Create substraction.c

---
+++ gitcprogram/substraction.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter any number to be substracted :",a);
+    scanf("%d",&a);
+    printf("enter number to be substracted:",b);
+    scanf("%d",&b);
+    printf("The difference of two number: %d",a-b);
+}
